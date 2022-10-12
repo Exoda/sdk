443e7be42229c61fd7af97f1ebeafac2978321b0
Bilal Ch <thered001@gmail.com>
2022-10-12 08:33:08 +0500

    fix: mainnet contract addresses

--- packages/core-sdk/src/constants/poolIds.ts
+++ packages/core-sdk/src/constants/poolIds.ts
@@ -2,7 +2,7 @@ import { PoolIdMap } from '../types'
 import { ChainId } from '../enums'
 
 export const FERMION_POOLID: PoolIdMap = {
-    [ChainId.ETHEREUM]: 5,
+    [ChainId.ETHEREUM]: 0,
     [ChainId.ROPSTEN]: 5,
     [ChainId.RINKEBY]: 5,
     [ChainId.GÖRLI]: 5,
